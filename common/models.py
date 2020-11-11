945f14c2576f4a3aad897c8498bb846f3d1417d0
Tamerlan Gudabyev <tamerlan@rocketfirm.com>
2020-11-11 06:32:23 +0600

    feat(common): add exercise models

--- common/models.py
+++ common/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from users.models import CustomUser
 
 
 class BaseModel(models.Model):
@@ -7,3 +8,21 @@ class BaseModel(models.Model):
 
     class Meta:
         abstract = True
+
+
+class MuscleType(BaseModel):
+    title = models.TextField()
+
+
+class Exercise(BaseModel):
+    title = models.TextField()
+    muscle_type = models.OneToOneField(MuscleType, on_delete=models.CASCADE)
+
+
+class CustomExercise(BaseModel):
+    title = models.TextField()
+    muscle_type = models.OneToOneField(MuscleType, on_delete=models.CASCADE)
+    user = models.OneToOneField(
+        CustomUser,
+        on_delete=models.CASCADE
+    )
